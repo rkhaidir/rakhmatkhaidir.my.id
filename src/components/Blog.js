ce80a04a07af9840db6335c5a8402edcc88ac2e5
rkhaidir <rkhaidir085@gmail.com>
2020-10-03 23:22:58 +0800

    edit component blog-1

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,7 +8,7 @@ export default class Blog extends Component {
                 <h1 className="title">BLOG</h1>
                 <div className="row offset-1">
                     <div className="col-md-4 blog">
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore ex dolorum molestias libero animi beatae....</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, enim amet distinctio perferendis rerum rem quos corrupti dolorem, quaerat velit ex perspiciatis quod reprehenderit voluptatum possimus consectetur iure, neque reiciendis.</p>
                         <img src="/img/blog.jpg" alt=""/>
                         <p className="blog-title"><b>Lorem ipsum dolor sit.</b> <br/> Jul 16, 2020</p>
                     </div>
