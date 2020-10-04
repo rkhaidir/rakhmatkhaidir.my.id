6a1bb2cf6b66252fed2f6adc12e5adbaf7400d96
rkhaidir <rkhaidir085@gmail.com>
2020-10-04 21:36:43 +0800

    fix router and fix pages blog

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Route, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './blog.css';
 
 export default class Blog extends Component {
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Route, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './navbar.css';
 
 export default class Navbar extends Component {
